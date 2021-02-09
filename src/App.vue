3c05e6197489dde7e98a8eb12f9228ae661e6dcd
Taran Jennison <Taran.Jennison@wineworks.co.nz>
2021-02-10 08:37:02 +1300

    Pre camera code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <b-container>
+            <b-row>
+                <b-col cols="5">
+                    <input
+                        class="standard-input"
+                        id="stockInput"
+                        v-model.number="stockCode"
+                        required
+                        @keydown="keyDown"
+                    >
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <b-button id="picButton" class="picButton" v-on:click="takePhoto()">Take photo</b-button>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    export default {
+        name: 'App',
+
+        data () {
+            return {
+                stockCode: ''
+            }
+        },
+
+        mounted() {
+            // Fetch image from q or receive from input link
+        },
+
+        methods: {
+            // On key press in input fields
+            // Filters any key press that is not 0-9 or 'ArrowRight','ArrowLeft','Backspace', 'Tab'
+            // (any filtered key presses are discarded)
+            // if key down is 'Tab', force update to fields without timeout (prevents updates being skipped by quick change of field resetting timeout)
+            keyDown(event) {
+                const validKeys = ['ArrowRight','ArrowLeft','Backspace', 'Tab', 'Enter'];
+                const keyRegex = /[0-9]/;
+                if (!keyRegex.test(event.key) && validKeys.indexOf(event.key) < 0) {
+                    event.preventDefault();
+                }
+            },
+        }
+    
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
